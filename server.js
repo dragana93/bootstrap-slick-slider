d7857a3a0aa7b8410d540d674ac46651729cbd23
draganasavanovic <dragana.savanovic@wayseven.com>
2019-11-29 09:52:33 +0100

    changed name of app

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<bootstrap-slick-slider>"));
+app.use(express.static(__dirname + "/dist/bootstrap-slick-slider"));
 
 app.get("/*", function(req, res) {
   res.sendFile(
-    path.join(__dirname + "/dist/<bootstrap-slick-slider>/index.html")
+    path.join(__dirname + "/dist/bootstrap-slick-slider/index.html")
   );
 });
 
